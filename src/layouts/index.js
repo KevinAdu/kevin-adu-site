333cb730578f9f40ea3c243388f471b134d3759c
KevinAdu <kevyugi@gmail.com>
2017-12-26 10:21:28 +0900

    Added markup for header

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -3,17 +3,28 @@ import Link from 'gatsby-link'
 
 import "../styles/main.scss"
 
+import profilePic from './profile-pic.jpg'
+import logo from './logo.svg'
+
 class Template extends React.Component {
   render() {
     const { location, children } = this.props
     let header
     if (location.pathname === '/') {
       header = (
-        <h1>
-          <Link to={'/'} >
-            Gatsby Starter Blog
-          </Link>
-        </h1>
+        <header className="hero">
+          <nav className="nav">
+            <object src={logo}></object>
+            <ul className="nav-links">
+              <Link to={'/'} >Life</Link>
+              <Link to={'/'} >Japan</Link>
+              <Link to={'/'} >About Me</Link>
+            </ul>
+          </nav>
+          <img className="hero-avatar" src={profilePic}></img>
+          <h1 className="hero-name">Kevin Adu-Sarkodie</h1>
+          <h2 className="hero-blurb">Expat in Japan; Web Developer; Enthusiast</h2>
+        </header>
       )
     } else {
       header = (
