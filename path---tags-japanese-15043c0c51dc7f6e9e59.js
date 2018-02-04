24aa3e5db8933b406e1abbd2396b59e22fcb4920
KevinAdu <kevyugi@gmail.com>
2018-02-04 21:57:36 +0900

    Updates

---
+++ path---tags-japanese-15043c0c51dc7f6e9e59.js
@@ -0,0 +1,2 @@
+webpackJsonp([0x85baeee0bb0c],{347:function(e,t){e.exports={data:{allMarkdownRemark:{totalCount:1,edges:[{node:{excerpt:"At the end of 2017, I took the  JLPT (Japanese Language Proficiency Test)  exam for the grade N2 for the third time. N2 is the second…",timeToRead:6,frontmatter:{title:"How I'm studying Japanese",date:"January 30, 2018",path:"/how-im-studying-japanese/"}}}]}},pathContext:{tag:"Japanese"}}}});
+//# sourceMappingURL=path---tags-japanese-15043c0c51dc7f6e9e59.js.map
---
+++ path---tags-language-6c6181206770371978ed.js
@@ -0,0 +1,2 @@
+webpackJsonp([0x7470efbeaada],{348:function(e,t){e.exports={data:{allMarkdownRemark:{totalCount:1,edges:[{node:{excerpt:"At the end of 2017, I took the  JLPT (Japanese Language Proficiency Test)  exam for the grade N2 for the third time. N2 is the second…",timeToRead:6,frontmatter:{title:"How I'm studying Japanese",date:"January 30, 2018",path:"/how-im-studying-japanese/"}}}]}},pathContext:{tag:"Language"}}}});
+//# sourceMappingURL=path---tags-language-6c6181206770371978ed.js.map
