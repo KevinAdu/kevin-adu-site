ce0c06d4b55c0e1593abcf64526f3837414e4bf4
KevinAdu <kevyugi@gmail.com>
2018-07-25 20:12:48 +0900

    Updates

---
+++ path----a49c291702b17363b395.js
@@ -0,0 +1,2 @@
+webpackJsonp([60335399758886],{112:function(e,t){e.exports={data:{allMarkdownRemark:{edges:[{node:{frontmatter:{category:"Life"}}},{node:{frontmatter:{category:"Life"}}},{node:{frontmatter:{category:"Japanese"}}},{node:{frontmatter:{category:"Life"}}},{node:{frontmatter:{category:"Life"}}},{node:{frontmatter:{category:"Life"}}},{node:{frontmatter:{category:"Life"}}},{node:{frontmatter:{category:"Life"}}},{node:{frontmatter:{category:"Life"}}}]}},layoutContext:{}}}});
+//# sourceMappingURL=path----a49c291702b17363b395.js.map
