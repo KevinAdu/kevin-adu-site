2eebf2349cc9cb7f754ffaebf13623e67a5a5cb5
KevinAdu <kevyugi@gmail.com>
2019-01-14 17:19:11 +0900

    Updates

---
+++ path----28399709d78976dfba51.js
@@ -0,0 +1,2 @@
+webpackJsonp([60335399758886],{112:function(e,t){e.exports={data:{allMarkdownRemark:{edges:[{node:{frontmatter:{category:"Life"}}},{node:{frontmatter:{category:"Life"}}},{node:{frontmatter:{category:"Life"}}},{node:{frontmatter:{category:"Life"}}},{node:{frontmatter:{category:"Life"}}},{node:{frontmatter:{category:"Japanese"}}},{node:{frontmatter:{category:"Life"}}},{node:{frontmatter:{category:"Life"}}},{node:{frontmatter:{category:"Life"}}},{node:{frontmatter:{category:"Life"}}}]}},layoutContext:{}}}});
+//# sourceMappingURL=path----28399709d78976dfba51.js.map
